3d828eab8b29da9e8ab94b02a9fc44fc82f3903b
Vinosh <57247481+vinoshg@users.noreply.github.com>
2023-11-08 07:08:54 +0530

    Add files via upload

---
+++ student/src/main/java/com/vinosh/student/StudentmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.vinosh.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmanagementsystemApplication.class, args);
+	}
+
+}
