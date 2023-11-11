59f9172dcd723f25665a859a14d5518dc7478221
Vinosh <57247481+vinoshg@users.noreply.github.com>
2023-11-11 18:56:16 +0530

    Update StudentRepository.java

--- Spring Boot CRUD/student/src/main/java/com/vinosh/student/StudentRepository.java
+++ Spring Boot CRUD/student/src/main/java/com/vinosh/student/StudentRepository.java
@@ -9,4 +9,8 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
   List<Student> findByName(String name); //Query DSL
   List<Student> findByNameOrderByIdDesc(String name);
   List<Student> findByNameOrderByIdAsc(String name);
+
+  //Or
+  @Query("from student where name= :name")
+  List<Student> find(@Param("name") String name);
 }
